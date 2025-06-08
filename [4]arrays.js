66f8914756fe27ecb1cea97e00f91a63959275ad
keedpek <zaharv20004@gmail.com>
2025-06-09 00:47:05 +0300

    feat: done task 4

--- [4]arrays.js
+++ [4]arrays.js
@@ -1,3 +1,25 @@
-//Напишите функцию, которая принимает массив с числами и находит сумму первой половины элементов этого массива.
+// Напишите функцию, которая принимает массив с числами и находит сумму первой половины элементов этого массива.
 
-//Решение
+// Решение
+function halfArraySum(arr) {
+  if (!Array.isArray(arr)) {return 'Ожидался массив'}
+  let acc = 0;
+  for (let i = 0; i < Math.ceil(arr.length / 2); i++) {
+    acc += arr[i];
+  }
+  return acc;
+}
+
+// Тесты
+
+const tests = [
+  {arr: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], expect: 15},
+  {arr: [11, 12, 13, 14, 15, 16, 17, 18, 19], expect: 65},
+  {arr: [], expect: 0},
+  {arr: [2], expect: 2},
+  {arr: 1, expect: 'Ожидался массив'},
+];
+
+tests.forEach((test, index) => {
+  console.log(halfArraySum(test.arr) === test.expect ? `Тест ${index + 1} пройден` : `Тест ${index + 1} не пройден`)
+})
