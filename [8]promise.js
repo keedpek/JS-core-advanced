8f71c650765e30caeb0d32b92d0fa975d916d6cb
keedpek <zaharv20004@gmail.com>
2025-06-09 19:25:05 +0300

    feat: done task 8

--- [8]promise.js
+++ [8]promise.js
@@ -1,3 +1,21 @@
-//Напишите функцию, которая принимает массив URL-адресов и параллельно загружает содержимое каждого URL-адреса с помощью Promises.
+// Напишите функцию, которая принимает массив URL-адресов и параллельно загружает содержимое каждого URL-адреса с помощью Promises.
 
-//Решение
+// Решение
+
+function fetchAllUrls(urls) {
+  const requests = urls.map(url => fetch(url)) 
+  return Promise.all(requests)
+    .then(responses => Promise.all(responses.map(res => res.json())))
+}
+
+const urls = [
+  'https://jsonplaceholder.typicode.com/posts/10',
+  'https://jsonplaceholder.typicode.com/posts/4',
+  'https://jsonplaceholder.typicode.com/posts/38',
+  'https://jsonplaceholder.typicode.com/posts/52',
+]
+
+fetchAllUrls(urls)
+  .then(data => {
+    data.forEach(post => {console.log(post)})
+  })
