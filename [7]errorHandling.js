28ce00fd6b3ed60026c37c54c0d6aaca003d2433
keedpek <zaharv20004@gmail.com>
2025-06-09 16:35:37 +0300

    feat: done task 7

--- [7]errorHandling.js
+++ [7]errorHandling.js
@@ -1,3 +1,24 @@
-//Напишите функцию, которая использует блок try-catch для перехвата и обработки ошибки TypeError при доступе к свойству неопределенного объекта.
+// Напишите функцию, которая использует блок try-catch для перехвата и обработки ошибки TypeError при доступе к свойству неопределенного объекта.
 
-//Решение
+// Решение
+
+function errorHandler(obj, prop) {
+  try {
+    let property = obj[prop];
+    return property
+  } catch (error) {
+    if (error instanceof TypeError) {
+      console.error(`TypeError: возникла ошибка при доступе к свойству неопределенного объекта`);
+    } else {
+      console.error(`Возникла неопределенная ошибка`);
+    }
+  }
+  return null;
+}
+
+const obj = {
+  name: 'Jakob',
+};
+
+console.log(errorHandler(obj, 'name'));
+console.log(errorHandler(null, 'age'));
