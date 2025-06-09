6cceae8597f4554a1fd6cf063ca4420d061207dd
keedpek <zaharv20004@gmail.com>
2025-06-09 22:34:35 +0300

    feat: done task 10

--- [10]eventLoop.js
+++ [10]eventLoop.js
@@ -1,3 +1,43 @@
-//Напишите функцию, которая извлекает данные из API и отменяет запрос, если он занимает больше указанного времени.
+// Напишите функцию, которая извлекает данные из API и отменяет запрос, если он занимает больше указанного времени.
 
-//Решение
+//TODO: AbortController() - отменяет сам HTTP запрос а не игнорирует результат
+// Решение
+
+function fetchData(url, runtime) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject('Превышено время ожидания ответа на запрос');
+    }, runtime);
+
+    //    runtimeErrorPromise - мок запрос длящийся 3 секунды (дольше переданных в функцию 2-х сек)
+
+    // const runtimeErrorPromise = new Promise((errResolve, errReject) => {
+    //   setTimeout(() => {
+    //     errResolve({
+    //       ok: true,
+    //       json: () => ({ id: 1, title: 'Mocked title' })
+    //     })
+    //   }, 3000);
+    // })
+
+    //    fetchPromise - обычный fetch запрос, должен успеть выполниться, при тестировании отменялся при переданном времени 170+-20 мс
+
+    const fetchPromise = fetch(url);
+
+    fetchPromise
+      .then(response => {
+        if (!response.ok) {
+          reject(`Непредвиденная ошибка ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(data => resolve(data))
+      .catch(err => console.error(err))
+  })
+}
+
+const url = 'https://jsonplaceholder.typicode.com/posts/10';
+
+fetchData(url, 1000)
+  .then(res => console.log(res))
+  .catch(err => console.error(`Ошибка ${err}`))
